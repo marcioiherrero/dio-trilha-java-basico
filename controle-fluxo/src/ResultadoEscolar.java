269b7131d82e7aa7e117d4ff8d77b31454d202a1
Marcio <marcioiherrero@gmail.com>
2023-10-03 12:21:46 -0300

    alterando condicional

--- controle-fluxo/src/ResultadoEscolar.java
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -2,12 +2,11 @@ public class ResultadoEscolar {
     
     public static void main(String[] args) {
         
-        int nota = 6;
+        int nota = 4;
         
-        if (nota >= 7)
-            System.out.println("Aprovado");
-        else 
-            System.out.println("Reprovado");
+        String resultado = nota >= 7 ? "Aprovado" : nota >= 5 && nota < 7 ? "Recuperacao" : "Reprovado";
+        
+        System.out.println(resultado);
         
     }
     
