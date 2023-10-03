b6c6644f56124e29ea4b98cb97b872ee0a396644
Marcio <marcioiherrero@gmail.com>
2023-10-03 12:46:55 -0300

    atualizando projeto

---
+++ controle-fluxo/src/PlanoOperadora.java
@@ -0,0 +1,22 @@
+public class PlanoOperadora {
+    
+	public static void main(String[] args) {
+		String plano = "M";
+        
+		switch (plano) {
+			case "T": {
+				System.out.println("5Gb Youtube");
+                break;
+			}
+			case "M": {
+				System.out.println("WhatsApp e Instagram gratis");
+                break;
+			}
+			case "B": {
+				System.out.println("100 minutos de ligacao");
+                break;
+			}
+		}
+	}
+    
+}
