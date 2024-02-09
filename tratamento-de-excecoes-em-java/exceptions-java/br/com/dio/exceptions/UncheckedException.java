a7a2b4b47d0cddc5577769abda5ee99769cb5d77
marcioiherrero <marcioiherrero@gmail.com>
2024-02-08 22:33:45 -0300

    Commits da aula de exceptions em java

---
+++ tratamento-de-excecoes-em-java/exceptions-java/br/com/dio/exceptions/UncheckedException.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+
+public class UncheckedException {
+    
+    public static void main(String[] args) {
+        
+        String a = JOptionPane.showInputDialog("Numerador: ");
+        String b = JOptionPane.showInputDialog("Denominador: ");
+        
+        int resultado = dividir(Integer.parseInt(a), Integer.parseInt(b));
+        
+        System.out.println("Resultado: " + resultado);
+        
+    }
+    
+    private static int dividir(int a, int b) {
+        return a / b;
+    }
+    
+}
