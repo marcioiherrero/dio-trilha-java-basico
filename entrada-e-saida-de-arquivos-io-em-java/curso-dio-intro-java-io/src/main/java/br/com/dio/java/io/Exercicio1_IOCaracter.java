3a11cbdfef2bd26d2afbc01168319414287b458a
marcioiherrero <marcioiherrero@gmail.com>
2024-02-13 20:53:03 -0300

    Mais commits da aula de IO com java.

---
+++ entrada-e-saida-de-arquivos-io-em-java/curso-dio-intro-java-io/src/main/java/br/com/dio/java/io/Exercicio1_IOCaracter.java
@@ -0,0 +1,41 @@
+package br.com.dio.java.io;
+
+import java.io.*;
+
+//Abra o teclado para escrever 3 filmes que você recomendaria e exiba esses filmes no console.
+public class Exercicio1_IOCaracter {
+    
+    public static void main(String[] args) throws IOException {
+        receberTecladoImprimirConsole();
+    }
+    
+    public static void receberTecladoImprimirConsole() throws IOException {
+        //InputStream is = System.in;
+        //InputStreamReader isr = new InputStreamReader(is);
+        //BufferedReader br = new BufferedReader(isr);
+        
+        System.out.println("Recomende 3 filmes: ");
+        
+        //padrão Decorator, simplifica tudo numa linha só...
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line = br.readLine();
+        
+        //OutputStream os = System.out;
+        //OutputStreamWriter osw = new OutputStreamWriter(os);
+        //BufferedWriter bw = new BufferedWriter(osw);
+        
+        //padrão Decorator, simplifica tudo numa linha só...
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        do {
+            bw.write(line);
+            bw.newLine();
+            line = br.readLine();
+        } while (!line.isEmpty());
+        bw.flush();
+        
+        br.close();
+        bw.close();
+    }
+    
+}
---
+++ entrada-e-saida-de-arquivos-io-em-java/curso-dio-intro-java-io/src/main/java/br/com/dio/java/io/Exercicio2_IOCaracter.java
@@ -0,0 +1,43 @@
+package br.com.dio.java.io;
+
+import java.io.*;
+import java.util.Scanner;
+
+//Abra o teclado para escrever 3 filmes que você recomendaria e armazene em "recomendados.txt"
+public class Exercicio2_IOCaracter {
+    
+    public static void main(String[] args) throws IOException {
+        lerTecladoEscreverDocumento();
+    }
+    
+    public static void lerTecladoEscreverDocumento() throws IOException {
+        
+        PrintWriter pw = new PrintWriter(System.out);
+        pw.println("Digite 3 recomendacoes de filmes: ");
+        pw.flush();
+        
+        Scanner scanner = new Scanner(System.in);
+        String line = scanner.nextLine();
+        
+        File f = new File("recomendacoes.txt");
+
+        BufferedWriter bw = new BufferedWriter(new FileWriter(f.getName()));
+        
+        do {
+            bw.write(line);
+            bw.newLine();
+            line = scanner.nextLine();
+        } while (!line.equalsIgnoreCase("fim"));
+        
+        bw.flush();
+        
+        pw.printf("Arquivo \"%s\" foi criado com sucesso!", f.getName());
+
+
+        bw.close();
+        scanner.close();
+        pw.close();
+        
+    }
+    
+}
